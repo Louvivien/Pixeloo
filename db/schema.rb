05747405d0146690e5238726ffb9c07e47d3edda
ratsimisampy <ratsimisampy@gmail.com>
2018-12-05 21:41:14 +0100

    updates related to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_154701) do
+ActiveRecord::Schema.define(version: 2018_12_05_072922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_154701) do
   create_table "line_items", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
@@ -79,6 +79,12 @@ ActiveRecord::Schema.define(version: 2018_11_28_154701) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.integer "type"
+    t.string "first_name"
+    t.string "last_name"
+    t.text "about"
+    t.string "img_cin_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
