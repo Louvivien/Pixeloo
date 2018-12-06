ba8dad2517036bbb157f5b93ede8bcdfeeb6f96c
Louvivien <vivien.richaud@gmail.com>
2018-12-06 16:00:29 +0100

    asset precompile

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Pixeloo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 5.2
 
     
