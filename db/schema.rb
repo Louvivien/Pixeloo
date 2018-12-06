cb9a3a301553f590f305f1b466cd421aae387ea4
ratsimisampy <ratsimisampy@gmail.com>
2018-12-06 15:46:33 +0100

    fixing image_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_133720) do
+ActiveRecord::Schema.define(version: 2018_12_06_144500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,13 +57,14 @@ ActiveRecord::Schema.define(version: 2018_12_06_133720) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.integer "category_id"
+    t.string "image_url"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "line_items", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
@@ -89,16 +90,14 @@ ActiveRecord::Schema.define(version: 2018_12_06_133720) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "superadmin_role", default: false
+    t.boolean "user_role", default: true
     t.string "username"
     t.string "first_name"
     t.string "last_name"
     t.text "about"
     t.string "img_cin_url"
-    t.boolean "superadmin_role", default: false
-    t.boolean "user_role", default: true
     t.string "user_type"
-    t.string "address"
-    t.integer "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
