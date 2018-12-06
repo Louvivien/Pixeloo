0fda468809501d1297da5d298a3a942c1c3c199e
ratsimisampy <ratsimisampy@gmail.com>
2018-12-06 14:19:32 +0100

    add column address and CP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_124516) do
+ActiveRecord::Schema.define(version: 2018_12_06_131209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,8 @@ ActiveRecord::Schema.define(version: 2018_12_06_124516) do
     t.boolean "superadmin_role", default: false
     t.boolean "user_role", default: true
     t.string "user_type"
+    t.string "address"
+    t.integer "postal_code"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
