1620f00b6c0ab68985319a411b3c7c0e6b387834
ratsimisampy <ratsimisampy@gmail.com>
2018-12-06 13:47:16 +0100

    update field user_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_230133) do
+ActiveRecord::Schema.define(version: 2018_12_06_124516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2018_12_05_230133) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "title"
+    t.string "img_url"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -82,13 +90,13 @@ ActiveRecord::Schema.define(version: 2018_12_05_230133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.integer "type"
     t.string "first_name"
     t.string "last_name"
     t.text "about"
     t.string "img_cin_url"
     t.boolean "superadmin_role", default: false
     t.boolean "user_role", default: true
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
