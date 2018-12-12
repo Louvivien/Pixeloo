7914d32eb508ad946b92b8f0c078ae9e7715df90
Alexisreau97 <alexis.reaupro@gmail.com>
2018-12-12 13:08:05 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 a = "In partes multatione vel militarium fovisse fovisse damnabatur suos postulatus vel hoc rumore multatione aut multatione beluae honoratorum fovisse suos modum bonorum catenarum rumore militarium aut insimulatus enim iniecto iniecto."
 
-user = User.create!(email: "xesh@alexis.Fr", password: "azerty")
+user = User.create!(email: "wesh@commentsava.fr", password: "azerty")
 
 1.times do |item|
 
