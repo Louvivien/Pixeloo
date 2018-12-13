3c6593229360aeffe6eb4aae84ba8f1cc1d3dafe
Louvivien <vivien.richaud@gmail.com>
2018-12-13 11:18:12 +0100

    change login show item & supp id sur cart et order

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-	  skip_before_action :authenticate_user!, :only => [:index]
+	  skip_before_action :authenticate_user!, :only => [:show]
 
   def index
     @categories = Category.all
