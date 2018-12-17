43eebb033e2e535357c58ea897671d63f86f5f8e
Louvivien <vivien.richaud@gmail.com>
2018-12-17 02:15:04 +0100

    modif droitscategories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-	  skip_before_action :authenticate_user!, :only => [:show]
+	  skip_before_action :authenticate_user!
 
   def index
     @categories = Category.all
