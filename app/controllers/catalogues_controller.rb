f806d415e07daf7d4b08bccd6d811feb7da7c1f5
Louvivien <vivien.richaud@gmail.com>
2018-12-17 01:58:21 +0100

    Category

--- app/controllers/catalogues_controller.rb
+++ app/controllers/catalogues_controller.rb
@@ -6,10 +6,15 @@ class CataloguesController < ApplicationController
       @location = params[:location]
       @search = params[:search]
 
-    if params[:search]
-        search = params[:search]
+  if params[:location].present?
+      search = params[:search]
       @nonordereditems =  Item.where("lower(title) LIKE '%#{search.downcase}%' OR lower(description) LIKE '%#{search.downcase}%'")
       @items = @nonordereditems.near(@location, 1000, order: 'distance') 
+      
+    elsif params[:search] 
+        search = params[:search]
+      @items =  Item.where("lower(title) LIKE '%#{search.downcase}%' OR lower(description) LIKE '%#{search.downcase}%'")
+    
 
       else
       @items = Item.all
